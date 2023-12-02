48574747266236d5d988f072cf8352806c932031
Yami Coders <121123471+yamicoders@users.noreply.github.com>
2023-12-03 01:17:49 +0530

    Update indexJavascript.js

--- public/indexJavascript.js
+++ public/indexJavascript.js
@@ -1,7 +1,3 @@
-// RSVP Functionality
-document.getElementById('rsvp-btn').addEventListener('click', function () {
-    alert('RSVP Successful!');
-});
 
 
 function menubtn() {
@@ -10,60 +6,6 @@ function menubtn() {
 function bar(x){
         x.classList.toggle("cross");
 }
-
-// <!-- Edit and Delete Buttons -->
-// function displayEvents() {
-//     const eventList = document.getElementById('event-list');
-//     eventList.innerHTML = ''; // Clear existing list
-
-//     events.forEach((event, index) => {
-//         const listItem = document.createElement('li');
-//         listItem.innerHTML = `<strong>${event.name}</strong> - ${event.date}, ${event.time} - ${event.location}<br>${event.description}
-//             <button onclick="editEvent(${index})">Edit</button>
-//             <button onclick="deleteEvent(${index})">Delete</button>`;
-//         eventList.appendChild(listItem);
-//     });
-// }
-
-function editEvent(index) {
-    // You can implement code to populate the form with existing event details for editing.
-    // For simplicity, let's just alert the event index for now.
-    alert(`Editing event at index ${index}`);
-}
-
-function deleteEvent(index) {
-    // You can implement code to remove the event from the events array.
-    // For simplicity, let's just alert the event index for now.
-    alert(`Deleting event at index ${index}`);
-}
-
-// Update the Edit Functionality
-
-    function editEvent(index) {
-        const event = events[index];
-
-        // Populate the form with existing event details for editing
-        document.getElementById('event-name').value = event.name;
-        document.getElementById('event-date').value = event.date;
-        document.getElementById('event-time').value = event.time;
-        document.getElementById('event-location').value = event.location;
-        document.getElementById('event-description').value = event.description;
-
-        // Optional: You can also change the 'Create Event' button to 'Update Event'
-        // For simplicity, let's just alert the event index for now.
-        alert(`Editing event at index ${index}`);
-    }
-
-    // Implement Delete Functionality
-    // Previous code...
-
-    function deleteEvent(index) {
-        // Remove the event from the events array
-        events.splice(index, 1);
-
-        // Update the displayed event list
-        displayEvents();
-    }
 
 
 
